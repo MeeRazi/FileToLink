0dc1b326b6a8eace80e9487d3fa8444295914864
ｒｙｍｅ <ryme.care@hotmail.com>
2023-11-11 01:20:26 +0530

    updated

--- plugins/main.py
+++ plugins/main.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from info import URL, BIN_CHANNEL
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from utils import temp
 
 
 @Client.on_message(filters.command("start") & filters.private)
@@ -14,10 +15,17 @@ async def private_receive_handler(client, message):
     msg = await client.send_cached_media(
         chat_id=BIN_CHANNEL,
         file_id=file_id.file_id)
+    
+
+    caption = await temp.BOT.get_messages(BIN_CHANNEL, msg.id)
+    caption = caption.caption
+    files_name = caption.replace("_", " ").replace("+", "").replace(".", " ").replace("mkv", "").replace("mp4", "")
+    print(files_name) 
+
     online = f"{URL}/watch/{msg.id}"
     download = f"{URL}/download/{msg.id}"
     await message.reply_text(
-        text=f"**Streaming Link:** \n{online}\n\n**Download Link:** \n{download}",
+        text=f"**File Name:\n{files_name}\nStreaming Link:** \n{online}\n\n**Download Link:** \n{download}",
         reply_markup=InlineKeyboardMarkup(
         [
             [
