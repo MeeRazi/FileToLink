caf61c2ced8eeb1a84bc6f392828206884f01352
ｒｙｍｅ <ryme.care@hotmail.com>
2023-11-11 01:04:44 +0530

    updated

--- plugins/main.py
+++ plugins/main.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-    await message.reply_text(f"Hello {message.from_user.mention},\n\nI am a Telegram Video Stream Bot. Send me any video and I will give you streaming & download link.")
+    await message.reply_text(f"**Hello {message.from_user.mention},\n\nI am a Telegram Video Stream Bot. Send me any video and I will give you streaming & download link.**")
 
 
 @Client.on_message((filters.private) & (filters.document | filters.video | filters.audio | filters.photo) , group=4)
@@ -14,8 +14,8 @@ async def private_receive_handler(client, message):
     msg = await client.send_cached_media(
         chat_id=BIN_CHANNEL,
         file_id=file_id.file_id)
-    online = f"{URL}watch/{msg.id}"
-    download = f"{URL}download/{msg.id}"
+    online = f"{URL}/watch/{msg.id}"
+    download = f"{URL}/download/{msg.id}"
     await message.reply_text(
         text=f"**Streaming Link:** \n{online}\n\n**Download Link:** \n{download}",
         reply_markup=InlineKeyboardMarkup(
